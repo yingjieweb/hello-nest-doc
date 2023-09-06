284b42c07f72e256a9f1b5cd3c3e14e7e054939f
yingjieweb <yingjieweb@qq.com>
2023-09-06 16:32:15 +0800

    feat: 🎸 Simplify sidebar config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Nest.js 入门及实践",
   description: "A documentation website for Hello Nest.",
-  base: "/hello-nest-doc/",
+  base: "/hello-nest-doc/", // publicPath
   themeConfig: {
     logo: "/images/logo.png",
     nav: [
@@ -11,96 +11,49 @@ module.exports = {
       },
     ],
     sidebar: [
+      ["/", "前言"],
       {
         title: "Nest.js 介绍",
         children: [
-          {
-            title: "可扩展？",
-            path: "/concept/expandable/",
-          },
-          {
-            title: "Express？",
-            path: "/concept/express/",
-          },
-          {
-            title: "哪些优势？",
-            path: "/concept/advantage/",
-          },
+          ["/concept/expandable/", "可扩展？"],
+          ["/concept/express/", "Express？"],
+          ["/concept/advantage/", "哪些优势？"],
         ],
       },
       {
         title: "Hello World!",
         children: [
-          {
-            title: "脚手架安装 & 项目初始化",
-            path: "/practice/init/",
-          },
-          {
-            title: "目录结构 & 文件介绍",
-            path: "/practice/intro/",
-          },
-          {
-            title: "相关装饰器介绍",
-            path: "/practice/decorators/",
-          },
+          ["/practice/init/", "脚手架安装 & 项目初始化"],
+          ["/practice/intro/", "目录结构 & 文件介绍"],
+          ["/practice/decorators/", "相关装饰器介绍"],
           {
             title: "CRUD 接口实现",
             children: [
-              {
-                title: "准备 user 模块",
-                path: "/practice/crud/module/",
-              },
-              {
-                title: "user.controller.ts",
-                path: "/practice/crud/controller/",
-              },
-              {
-                title: "user.service.ts",
-                path: "/practice/crud/service/",
-              },
+              ["/practice/crud/module/", "准备 user 模块"],
+              ["/practice/crud/controller/", "user.controller.ts"],
+              ["/practice/crud/service/", "user.service.ts"],
             ],
           },
           {
             title: "参数校验",
             children: [
-              {
-                title: "Body、Query 参数校验",
-                path: "/practice/paramsDto/body&query/",
-              },
-              {
-                title: "Params 参数校验",
-                path: "/practice/paramsDto/params/",
-              },
+              ["/practice/paramsDto/body&query/", "Body、Query 参数校验"],
+              ["/practice/paramsDto/params/", "Params 参数校验"],
             ],
           },
           {
             title: "设置状态码",
             children: [
-              {
-                title: "通过 @HttpCode() 装饰器设置",
-                path: "/practice/statusCode/httpCode/",
-              },
-              {
-                title: "通过 @Res() 参数装饰器设置",
-                path: "/practice/statusCode/res/",
-              },
-              {
-                title: "二者的区别",
-                path: "/practice/statusCode/diff/",
-              },
+              ["/practice/statusCode/httpCode/", "通过 @HttpCode() 装饰器设置"],
+              ["/practice/statusCode/res/", "通过 @Res() 参数装饰器设置"],
+              ["/practice/statusCode/diff/", "二者的区别"],
             ],
           },
           {
             title: "统一响应体结构",
             children: [
-              {
-                title: "封装统一响应工具函数",
-                path: "/practice/response/utils/",
-              },
-              {
-                title: "封装统一响应过滤器",
-                path: "/practice/response/filters/",
-              },
+              ["/practice/response/utils/", "封装统一响应工具函数"],
+              ["/practice/response/filters/", "封装统一响应过滤器"],
             ],
           },
         ],
@@ -108,24 +61,12 @@ module.exports = {
       {
         title: "配置 Swagger",
         children: [
-          {
-            title: "安装 & 配置",
-            path: "/swagger/install/",
-          },
-          {
-            title: "装饰器介绍",
-            path: "/swagger/decorators/",
-          },
-          {
-            title: "配置示例",
-            path: "/swagger/examples/",
-          },
+          ["/swagger/install/", "安装 & 配置"],
+          ["/swagger/decorators/", "装饰器介绍"],
+          ["/swagger/examples/", "配置示例"],
         ],
       },
-      {
-        title: "数据库集成",
-        path: "/database/",
-      },
+      ["/database/", "数据库集成"],
     ],
   },
 };
